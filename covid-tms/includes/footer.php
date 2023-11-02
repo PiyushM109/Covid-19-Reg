638915d0d255d94f7afa75d8adc5177b7469bde1
Radhakrushna Mahadik <mahdikrs512@gmail.com>
2023-11-02 20:54:01 +0530

    first commit

---
+++ covid-tms/includes/footer.php
@@ -0,0 +1,9 @@
+ <!-- Footer -->
+            <footer class="sticky-footer bg-dark">
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto" style="color:#fcfcfc">
+                        <span>Copyright &copy; github.com/rk-3131 2023</span>
+                    </div>
+                </div>
+            </footer>
+            <!-- End of Footer -->
---
+++ covid-tms/index.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Covid-19 Testing Management System</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/scrolling-nav.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav" style="background-image: url('img/sidebar_bg.jpg');">
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">C-19 Portal</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#about" style="color:#fff">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#services" style="color:#fff">Symptoms</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#contact" style="color:#fff">Prevention</a>
+          </li>
+  <li class="nav-item active">
+            <a class="nav-link js-scroll-trigger" href="new-user-testing.php">Testing</a>
+          </li>
+           <li class="nav-item active">
+            <a class="nav-link js-scroll-trigger" href="live-test-updates.php">Live Updates</a>
+          </li>
+
+           <li class="nav-item active">
+            <a class="nav-link js-scroll-trigger" href="login.php">Admin</a>
+          </li>
+
+
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <header class="bg-primary text-white" style="background-image: url('img/bg_cv.jpg');">
+    <div class="container text-center">
+      <h1 style="color:black;">COVID19 TEST</h1>
+      <p class="lead"  style="color:black;">COVID19 - Testing Management System</p>
+    </div>
+  </header>
+<div class="middle"  style="background-image: url('img/bg_middle.jpg');">
+  <section id="about">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+          <h2 class="lead">About Covid 19</h2>
+          <p class="lead">COVID-19 is a highly contagious respiratory illness caused by the novel coronavirus SARS-CoV-2, first identified in Wuhan, China, in December 2019. Symptoms include fever, cough, shortness of breath, and loss of taste or smell, with some individuals remaining asymptomatic. The virus primarily spreads through respiratory droplets and by touching contaminated surfaces. Preventative measures include wearing masks, physical distancing, handwashing, and vaccination. Variants of the virus have emerged, requiring ongoing monitoring.</p>
+          <p class="lead"> The pandemic has had a profound impact on public health, economies, and daily life. Multiple COVID-19 vaccines have been developed and authorized for use globally, helping to reduce the severity of the disease. There is no specific antiviral treatment, but supportive care is used for severe cases. Some individuals experience "long COVID" with lingering symptoms. A global response involves governments, healthcare organizations, and researchers working together to combat the pandemic and provide guidance to the public..</p>
+         
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="services">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+          <h2>Covid-19 Symptoms</h2>
+          <hr />
+          <p><strong>COVID-19 Symptoms:</strong></p>
+<p><strong>Fever:</strong> Reported illnesses have ranged from mild symptoms to severe illness and death.</p>
+<br><hr>
+<p><strong>Cough:</strong> Reported illnesses have ranged from mild symptoms to severe illness and death.</p><br><hr>
+<p><strong>Shortness of breath:</strong> Reported illnesses have ranged from mild symptoms to severe illness and death.</p><br><hr>
+<p><strong>Fatigue:</strong> Reported illnesses have ranged from mild symptoms to severe illness and death.</p><br><hr>
+<p><strong>Body aches:</strong> Reported illnesses have ranged from mild symptoms to severe illness and death.</p><br><hr>
+<p><strong>Loss of taste or smell:</strong> Reported illnesses have ranged from mild symptoms to severe illness and death.</p><br><hr>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="contact">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+          <h2>Prevention</h2>
+  <ul>
+  <li>Wear a mask over your nose and mouth in public settings.</li>
+    <li>Maintain physical distance of at least 6 feet (2 meters) from others.</li>
+    <li>Wash your hands frequently with soap and water for at least 20 seconds.</li>
+    <li>Use hand sanitizer with at least 60% alcohol when soap and water are not available.</li>
+    <li>Avoid touching your face, especially your eyes, nose, and mouth.</li>
+    <li>Cover your mouth and nose with a tissue or your elbow when you cough or sneeze.</li>
+    <li>Clean and disinfect frequently-touched surfaces daily.</li>
+    <li>Stay home and self-isolate if you are feeling unwell or have COVID-19 symptoms.</li>
+    <li>Get vaccinated when eligible to protect yourself and others.</li>
+    <li>Follow public health guidelines and regulations in your area.</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+  </div>
+  <!-- Footer -->
+  <footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; github.com/rk-3131 2023</p>
+    </div>
+    <!-- /.container -->
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom JavaScript for this theme -->
+  <script src="js/scrolling-nav.js"></script>
+
+</body>
+
+</html>
